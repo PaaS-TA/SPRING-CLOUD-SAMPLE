8412afcf603d56b3c9052656d933798517d2cfb2
dojjang <betterpoul@gmail.com>
2018-07-12 16:53:06 +0900

    API-TWO rest 전용 controller 생성

--- SPRING-UI/src/main/java/org/openpaas/paasta/portal/web/controller/ExamController.java
+++ SPRING-UI/src/main/java/org/openpaas/paasta/portal/web/controller/ExamController.java
@@ -1,20 +1,17 @@
 package org.openpaas.paasta.portal.web.controller;
 
-import com.sun.tools.javac.util.List;
+
 import org.openpaas.paasta.portal.web.service.ExamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -37,6 +34,7 @@ public class ExamController {
 
     @GetMapping(value = {"/eureka/{index}"})
     public Map eurekaIndex(@PathVariable String index, HttpServletRequest request) {
+        LOGGER.info("eurekaIndex start");
         LOGGER.info("eurekaIndex :: " + index);
         Map rs = examService.eurekaRest("CLOUDAPIONE/apps/" + index + "/summary", HttpMethod.GET, null);
         return rs;
@@ -44,8 +42,23 @@ public class ExamController {
 
     @GetMapping(value = {"/zuul/{index}"})
     public Map zuulIndex(@PathVariable String index, HttpServletRequest request) {
+        LOGGER.info("zuulIndex start");
         LOGGER.info("eurekaIndex :: " + index);
         Map rs = examService.zuulRest("cloudapione/apps/" + index + "/summary", HttpMethod.GET, null);
+        return rs;
+    }
+
+    @GetMapping(value = {"/eureka/servicePackCatalog/list"})
+    public Map eurekaGetServicePackCatalogList(HttpServletRequest request) {
+        LOGGER.info("eurekaGetServicePackCatalogList start");
+        Map rs = examService.eurekaRest("CloudApiTwo/servicePackCatalog/list", HttpMethod.GET, null);
+        return rs;
+    }
+
+    @GetMapping(value = {"/zuul/servicePackCatalog/list"})
+    public Map zuulGetServicePackCatalogList(HttpServletRequest request) {
+        LOGGER.info("zuulGetServicePackCatalogList start");
+        Map rs = examService.zuulRest("CloudApiTwo/servicePackCatalog/list", HttpMethod.GET, null);
         return rs;
     }
 
