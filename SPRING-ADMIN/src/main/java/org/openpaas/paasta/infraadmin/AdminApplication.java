92ce6e271199b8f8e818c30a447382f2c2e3f2ef
dojjang <betterpoul@gmail.com>
2018-07-13 09:46:36 +0900

    주석

--- SPRING-ADMIN/src/main/java/org/openpaas/paasta/infraadmin/AdminApplication.java
+++ SPRING-ADMIN/src/main/java/org/openpaas/paasta/infraadmin/AdminApplication.java
@@ -8,11 +8,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * The type Application.
+ * SpringBootApplication
+ * Configuration, EnableAutoConfiguration, ComponetScan 이 포함되어있다.
+ *
+ * EnableAdminServer
+ * AdminServer 로 사용 할것을 선언한다.
+ *
+ * EnableDiscoveryClient
+ * EurekaClient 를 등록한다.
  */
-
-@EnableAutoConfiguration
-@Configuration
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
