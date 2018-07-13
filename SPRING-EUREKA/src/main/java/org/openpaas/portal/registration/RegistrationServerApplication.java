54ff38584da60a89261e2ee1fd2a794188318de9
dojjang <betterpoul@gmail.com>
2018-07-13 09:47:50 +0900

    주석

--- SPRING-EUREKA/src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
+++ SPRING-EUREKA/src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
@@ -7,9 +7,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * All you need to run a Eureka registration server.
- * 
- * @author Paul Chapman
+ * SpringBootApplication
+ * Configuration, EnableAutoConfiguration, ComponetScan 이 포함되어있다.
+ *
+ * EnableEurekaServer
+ * EurekaServer 로 사용 할것을 선언한다.
+ *
+ * EnableDiscoveryClient
+ * EurekaClient 를 등록한다.
  */
 @SpringBootApplication
 @EnableEurekaServer
