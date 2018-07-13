777036b2601a3fd7f31b796a052200ce09af7abc
dojjang <betterpoul@gmail.com>
2018-07-13 09:47:25 +0900

    주석

--- SPRING-CONFIG-SERVER/src/main/java/org/openpaas/paasta/configserver/ConfigServerApplication.java
+++ SPRING-CONFIG-SERVER/src/main/java/org/openpaas/paasta/configserver/ConfigServerApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
 /**
- * The type Application.
+ * EnableConfigServer
+ * ConfigServer 로 사용 할것을 선언.
+ *
+ * SpringBootApplication
+ * Configuration, EnableAutoConfiguration, ComponetScan 이 포함되어있다.
  */
-
-
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
